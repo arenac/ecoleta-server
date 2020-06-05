280106c1278ac4e32f5b3295d10f8ef30200f499
Nilo Neregato <neregato.nilo@gmail.com>
2020-06-05 22:57:30 +0200

    DB
    -Knex as ORM;
    -sqlite3 as DB for PoC;
    -create schemas through migrations;

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,13 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 
+import routes from './routes';
+
 const app = express();
 
+app.use(express.json());
 app.use(cors());
+app.use(routes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listen at ${process.env.BASE_URL}:${process.env.PORT} 🚀️`);
